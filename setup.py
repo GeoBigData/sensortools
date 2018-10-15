3e9134c73f27d17b5daf8b41e296921e6465b731
alopez <alopez@alopezs-MacBook-Pro.local>
2018-10-15 13:52:25 -0600

    error in find packages

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'Programming Language :: Python :: 2.7',
     ],
 
-    packages=find_packages(['sensortools'], exclude=['tests', 'docs', 'examples']),
+    packages=find_packages(exclude=['tests', 'docs', 'examples']),
 
     install_requires=requirements
 
