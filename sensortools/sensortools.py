285f6a46cacb2c8c7792aa1ee085584b52bd156a
alopez <alopez@alopezs-MacBook-Pro.local>
2018-10-26 15:33:38 -0600

    added aoi cloud cover function

--- sensortools/sensortools.py
+++ sensortools/sensortools.py
@@ -204,6 +204,72 @@ class sensortools(object):
 
         return [pct, inter_json]
 
+    def aoiCloudCover(self, df, aoi):
+        """
+        For each footprint in the search results, calculate a percent cloud
+        cover for the AOI (instead of entire strip)
+        """
+        try:
+            with ('~/.duc-api.txt', 'r') as a:
+                api_key = a.readlines()[0].rstrip()
+        except:
+            print('Could not find DUC API key in ~/.duc-api.txt')
+        # search the results, do not submit catids with 0 cloud cover
+        catids = df[df['Cloud Cover'] > 0].catalog_id.values
+
+        # send request to DUC database
+        url = "https://api.discover.digitalglobe.com/v1/services/cloud_cover/MapServer/0/query"
+        headers = {
+            'x-api-key': "{api_key}".format(api_key=api_key),
+            'content-type': "application/x-www-form-urlencoded"
+        }
+        data = {
+            'outFields': '*',
+            'where':"image_identifier IN ({cat})".format(cat="'" + "','".join(catids) + "'")
+            'outSR':'4326',
+            'f':'geojson'
+        }
+        response = requests.request("POST", url, headers=headers, data=data)
+        clouds = json.loads(response.text)
+
+        # projection info
+        to_p = self._getUTMProj(aoi)
+        from_p = pyproj.Proj(init='epsg:4326')
+        project = partial(pyproj.transform, from_p, to_p)
+
+        # project the AOI, calc area
+        aoi_shp = shapely.wkt.loads(aoi)
+        aoi_shp_prj = transform(project, aoi_shp)
+        aoi_km2 = aoi_shp_prj.area / 1000000.
+
+        # add column to search df
+        df['AOI Cloud Cover'] = 0
+
+        # iterate over the clouds and perform cloud cover percent
+        for feature in clouds['features']:
+            # extract the clouds and conver to shape
+            cloud = shapely.geometry.shape(feature['geometry'])
+
+            # get catalog id
+            c = feature['properties']['image_identifier']
+
+            # project the clouds
+            cloud_prj = transform(project, cloud)
+
+            # perform intersection and calculate area
+            try:
+                inter_shp_prj = aoi_shp_prj.intersection(cloud_prj)
+            except:
+                cloud_prj = cloud_prj.buffer(0.0)
+                inter_shp_prj = aoi_shp_prj.intersection(cloud_prj)
+            inter_km2 = inter_shp_prj.area / 1000000.
+            pct = inter_km2 / aoi_km2 * 100.
+
+            # update the dataframe
+            df.loc[df['catalog_id']==c, 'AOI Cloud Cover'] = pct
+
+        return df
+
     def aoiFootprintPctCoverage(self, df, aoi):
         """
         Return the percent area covered from aoi footprint calculation
