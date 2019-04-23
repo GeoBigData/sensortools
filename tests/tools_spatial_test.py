7cfeafe318126e1393a5a52c8546245f4262f051
rwegener2 <rachel.e.wegener@gmail.com>
2019-04-23 17:03:59 -0600

    updated tests with decorators

--- tests/tools_spatial_test.py
+++ tests/tools_spatial_test.py
@@ -1,5 +1,8 @@
 import sensortools.tools.spatial as spatial_tools
+from sensortools.decorators import InputError
+from shapely.geometry import Point
 from pyproj import Proj
+import pytest
 
 
 def test_convertaoitolocation():
@@ -7,6 +10,12 @@ def test_convertaoitolocation():
     y, x = spatial_tools.convertAOItoLocation(aoi_wkt)
     assert x == 12.5
     assert y == -2.5
+
+
+def test_convertaoitolocation_input_err():
+    with pytest.raises(InputError):
+        aoi = (15, -5, 15, 0, 10, 0, 10, -5, 15, -5)
+        spatial_tools.convertAOItoLocation(aoi)
 
 
 def test_getUTMProj_north():
@@ -21,6 +30,12 @@ def test_getUTMProj_south():
     assert spatial_tools.getUTMProj(maputo).srs == truth_proj.srs
 
 
+def test_getUTMProj_input_err():
+    with pytest.raises(InputError):
+        aoi = (15, -5, 15, 0, 10, 0, 10, -5, 15, -5)
+        spatial_tools.getUTMProj(aoi)
+
+
 def test_getLLUTMProj_north():
     madrid = [40.4168, -3.7038]
     truth_proj = Proj(proj='utm', zone=30, ellps='WGS84', hemisphere='north')
@@ -31,3 +46,21 @@ def test_getLLUTMProj_south():
     maputo = [-25.9692, 32.5732]
     truth_proj = Proj(proj='utm', zone=36, ellps='WGS84', hemisphere='south')
     assert spatial_tools.getLLUTMProj(*maputo).srs == truth_proj.srs
+
+
+def test_getLLUTMProj_inputtype_err():
+    with pytest.raises(InputError):
+        aoi = Point(-3.7038, 40.4168).coords.xy
+        spatial_tools.getLLUTMProj(*aoi)
+
+
+def test_getLLUTMProj_lat_bound_err():
+    with pytest.raises(InputError):
+        aoi = [309483, -180]
+        spatial_tools.getLLUTMProj(*aoi)
+
+
+def test_getLLUTMProj_lon_bound_err():
+    with pytest.raises(InputError):
+        aoi = ['42', '500392']
+        spatial_tools.getLLUTMProj(*aoi)
