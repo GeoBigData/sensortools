11ae611a6c928000cf1057d7f067cd1c31ccb324
rwegener2 <rachel.e.wegener@gmail.com>
2019-05-06 13:12:14 -0600

    removing DUC API key references

--- sensortools/exceptions.py
+++
@@ -1,8 +0,0 @@
-class MissingDUCAPIkeyError(Exception):
-    """Raised when a ./duc-api.txt cannot be found"""
-    pass
-
-
-class DUCAPIkeyFormattingError(Exception):
-    """Raised when DUC key cannot be extracted from ./duc-api.txt"""
-    pass
